feacc99b74ebd754d16965706a9a6ef9e773875b
Despoina Paschalidou <paschalidoud@gmail.com>
2024-06-16 23:41:00 -0700

    Add accepted papers

---
+++ make-paper-list.py
@@ -0,0 +1,165 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+
+import argparse
+from collections import namedtuple
+import sys
+
+
+def _print(args, text, **kwargs):
+    if not args.quiet:
+        print(text, **kwargs)
+
+
+class Paper(namedtuple("Paper", [
+        "title",
+        "url",
+        "authors",
+        "links"
+    ])):
+    pass
+
+
+class Conference(namedtuple("Conference", ["name"])):
+    pass
+
+
+class Link(namedtuple("Link", ["name", "url", "html", "text"])):
+    pass
+
+
+def author_list(authors):
+    return authors.split(",")
+
+
+publications = [
+    Paper(
+        "Consistency^2: Consistent and Fast 3D Painting with Latent Consistency Models",
+        "papers/paper_21.pdf",
+        author_list("Wang, Tianfu; Obukhov, Anton; Schindler, Konrad"),
+        [ Link("Abstract", None, "Generative 3D Painting is among the top productivity boosters in high-resolution 3D asset management and recy- cling. Ever since text-to-image models became accessible for inference on consumer hardware, the performance of 3D Painting methods has consistently improved and is cur- rently close to plateauing. At the core of most such models lies denoising diffusion in the latent space, an inherently time-consuming iterative process. Multiple techniques have been developed recently to accelerate generation and reduce sampling iterations by orders of magnitude. Designed for 2D generative imaging, these techniques do not come with recipes for lifting them into 3D. In this paper, we ad- dress this shortcoming by proposing a Latent Consistency Model (LCM) adaptation for the task at hand. We analyze the strengths and weaknesses of the proposed model and evaluate it quantitatively and qualitatively. Based on the Objaverse dataset samples study, our 3D painting method attains strong preference in all evaluations.", None),
+            Link("Paper", "papers/paper_21.pdf", None, None),
+            Link("Poster", "posters/poster_21.pdf", None, None)
+        ]
+    ),
+
+    Paper(
+        "StyLitGAN: Image-based Relighting via Latent Control",
+        "https://openaccess.thecvf.com/content/CVPR2024/papers/Bhattad_StyLitGAN_Image-Based_Relighting_via_Latent_Control_CVPR_2024_paper.pdf",
+        author_list("Bhattad, Anand; Soole, James; Forsyth, David"),
+        [ Link("Abstract", None, "We describe a novel method, StyLitGAN, for relighting and resurfacing images in the absence of labeled data. StyLitGAN generates images with realistic lighting effects, including cast shadows, soft shadows, inter-reflections, and glossy effects, without the need for paired or CGI data. StyLitGAN uses an intrinsic image method to decompose an image, followed by a search of the latent space of a pretrained StyleGAN to identify a set of directions. By prompting the model to fix one component (e.g., albedo) and vary another (e.g., shading), we generate relighted images by adding the identified directions to the latent style codes. Quantitative metrics of change in albedo and lighting diversity allow us to choose effective directions using a forward selection process. Qualitative evaluation confirms the effectiveness of our method.", None),
+            Link("Paper", "https://openaccess.thecvf.com/content/CVPR2024/papers/Bhattad_StyLitGAN_Image-Based_Relighting_via_Latent_Control_CVPR_2024_paper.pdf", None, None),
+        ]
+    ),
+
+]
+
+
+def build_publications_list(publications):
+    def image(paper):
+        if paper.image is not None:
+            return '<img src="{}" alt="{}" />'.format(
+                paper.image, paper.title
+            )
+        else:
+            return '&nbsp;'
+
+    def title(paper):
+        return '<a href="{}">{}</a>'.format(paper.url, paper.title)
+
+    def authors(paper):
+        return ", ".join(a for a in paper.authors)
+
+    def links(paper):
+        def links_list(paper):
+            def link(i, link):
+                if link.url is not None:
+                    # return '<a href="{}">{}</a>'.format(link.url, link.name)
+                    return '<a href="{}" data-type="{}">{}</a>'.format(link.url, link.name, link.name)
+                else:
+                    return '<a href="#" data-type="{}" data-index="{}">{}</a>'.format(link.name, i, link.name)
+            return " ".join(
+                link(i, l) for i, l in enumerate(paper.links)
+            )
+
+        def links_content(paper):
+            def content(i, link):
+                if link.url is not None:
+                    return ""
+                return '<div class="link-content" data-index="{}">{}</div>'.format(
+                    i, link.html if link.html is not None
+                       else '<pre>' + link.text + "</pre>"
+                )
+            return "".join(content(i, link) for i, link in enumerate(paper.links))
+        return links_list(paper) + links_content(paper)
+
+    def paper(p):
+        return ('<div class="row paper">'
+                    '<div class="content">'
+                        '<div class="paper-title">{}</div>'
+                        '<div class="authors">{}</div>'
+                        '<div class="links">{}</div>'
+                    '</div>'
+                '</div>').format(
+                title(p),
+                authors(p),
+                links(p)
+            )
+
+    return "".join(paper(p) for p in publications)
+
+
+def main(argv):
+    parser = argparse.ArgumentParser(
+        description="Create a publication list and insert in into an html file"
+    )
+    parser.add_argument(
+        "file",
+        help="The html file to insert the publications to"
+    )
+
+    parser.add_argument(
+        "--safe", "-s",
+        action="store_true",
+        help="Do not overwrite the file but create one with suffix .new"
+    )
+    parser.add_argument(
+        "--quiet", "-q",
+        action="store_true",
+        help="Do not output anything to stdout/stderr"
+    )
+
+    args = parser.parse_args(argv)
+
+    # Read the file
+    with open(args.file) as f:
+        html = f.read()
+
+    # Find the fence comments
+    start_text = "<!-- start paper list -->"
+    end_text = "<!-- end paper list -->"
+    start = html.find(start_text)
+    end = html.find(end_text, start)
+    if end < start or start < 0:
+        _print(args, "Could not find the fence comments", file=sys.stderr)
+        sys.exit(1)
+
+    # Build the publication list in html
+    replacement = build_publications_list(publications)
+
+    # Update the html and save it
+    html = html[:start+len(start_text)] + replacement + html[end:]
+
+    # If safe is set do not overwrite the input file
+    if args.safe:
+        with open(args.file + ".new", "w") as f:
+            f.write(html)
+    else:
+        with open(args.file, "w") as f:
+            f.write(html)
+
+
+if __name__ == "__main__":
+    main(None)
