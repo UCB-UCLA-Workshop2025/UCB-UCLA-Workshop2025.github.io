f256f0a9c03b528a7db16ee5e25ebe6f0e81d68f
Despoina Paschalidou <paschalidoud@gmail.com>
2024-06-17 00:31:19 -0700

    Add paper 34

--- make-paper-list.py
+++ make-paper-list.py
@@ -95,6 +95,15 @@ publications = [
         ]
     ),
 
+    Paper(
+        "ART3D: 3D Gaussian Splatting for Text-Guided Artistic Scenes Generation",
+        "https://arxiv.org/pdf/2405.10508",
+        author_list("Li, Pengzhi; Tang, Chengshuai; Huang, Qinxuan; Li, Zhiheng"),
+        [ Link("Abstract", None, "In this paper, we explore the existing challenges in 3D artistic scene generation by introducing ART3D, a novel framework that combines diffusion models and 3D Gaussian splatting techniques. Our method effectively bridges the gap between artistic and realistic images through an innovative image semantic transfer algorithm. By leveraging depth information and an initial artistic image, we generate a point cloud map, addressing domain differences. Additionally, we propose a depth consistency module to enhance 3D scene consistency. Finally, the 3D scene serves as initial points for optimizing Gaussian splats. Experimental results demonstrate ART3D's superior performance in both content and structural consistency metrics when compared to existing methods. ART3D significantly advances the field of AI in art creation by providing an innovative solution for generating high-quality 3D artistic scenes.", None),
+            Link("Paper", "https://arxiv.org/pdf/2405.10508", None, None),
+            Link("Poster", "posters/poster_34.pdf", None, None)
+        ]
+    ),
 
     Paper(
         "StyLitGAN: Image-based Relighting via Latent Control",
